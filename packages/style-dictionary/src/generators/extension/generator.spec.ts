bc32cd2fe738b6a614f2e0d59c9520f52a05827c
Sebastiandg7 <13395979+sebastiandg7@users.noreply.github.com>
2022-10-28 22:55:04 -0500

    feat(style-dictionary): extensions generator

--- packages/style-dictionary/src/generators/extension/generator.spec.ts
+++ packages/style-dictionary/src/generators/extension/generator.spec.ts
@@ -2,11 +2,15 @@ import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import { Tree, readProjectConfiguration } from '@nrwl/devkit';
 import { createLib } from '../../utils/testing/generators';
 import generator from './generator';
-import { ExtensionGeneratorSchema } from './schema';
+import { Extension, ExtensionGeneratorSchema } from './schema';
 
 describe('extension generator', () => {
   let appTree: Tree;
-  const options: ExtensionGeneratorSchema = { project: 'test' };
+  const options: ExtensionGeneratorSchema = {
+    project: 'test',
+    extensions: [],
+    directory: 'src/extensions',
+  };
 
   beforeEach(async () => {
     appTree = createTreeWithEmptyWorkspace();
@@ -14,8 +18,15 @@ describe('extension generator', () => {
   });
 
   it('should run successfully', async () => {
+    options.extensions = [Extension.ACTIONS, Extension.FILTERS];
     await generator(appTree, options);
-    const config = readProjectConfiguration(appTree, 'test');
+    const config = readProjectConfiguration(appTree, options.project);
     expect(config).toBeDefined();
+    expect(config.targets.build.options.customActions).toBe(
+      `libs/${options.project}/src/extensions/actions/index.ts`
+    )
+    expect(config.targets.build.options.customFilters).toBe(
+      `libs/${options.project}/src/extensions/filters/index.ts`
+    );
   });
 });
