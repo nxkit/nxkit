285afdcf7815010907fb4feb209ae3cebc6bd87d
Sebastiandg7 <13395979+sebastiandg7@users.noreply.github.com>
2023-01-03 22:15:04 -0500

    feat(playwright): generator tests

--- packages/playwright/src/generators/project/generator.spec.ts
+++ packages/playwright/src/generators/project/generator.spec.ts
@@ -1,14 +1,14 @@
-import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import {
-  Tree,
-  readProjectConfiguration,
   addProjectConfiguration,
+  readProjectConfiguration,
+  Tree,
 } from '@nrwl/devkit';
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 
 import { projectGenerator } from './generator';
 import { ProjectGeneratorSchema } from './schema';
 
-describe('project generator', () => {
+describe('Playwright Project', () => {
   let appTree: Tree;
   const options: ProjectGeneratorSchema = { name: 'test' };
 
@@ -16,16 +16,85 @@ describe('project generator', () => {
     appTree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
   });
 
-  it('should create standalone e2e project', async () => {
-    await projectGenerator(appTree, options);
-    const config = readProjectConfiguration(appTree, 'test-e2e');
-    expect(config).toBeDefined();
+  describe('standalone', () => {
+    it('should add the project configuration', async () => {
+      const projectName = options.name + '-e2e';
+      await projectGenerator(appTree, options);
+
+      const project = readProjectConfiguration(appTree, options.name);
+      expect(project).toBeDefined();
+
+      expect(project.targets.e2e).toEqual({
+        executor: '@nxkit/playwright:test',
+        outputs: ['{options.outputPath}'],
+        options: {
+          outputPath: `dist/apps/${projectName}/test-results`,
+          playwrightConfig: `apps/${projectName}/playwright.config.ts`,
+          baseUrl: 'https://example.com',
+        },
+        configurations: {
+          production: {
+            baseUrl: 'https://example.com',
+          },
+        },
+      });
+
+      expect(project.targets.debug).toEqual({
+        executor: '@nxkit/playwright:test',
+        outputs: ['{options.outputPath}'],
+        options: {
+          outputPath: `dist/apps/${projectName}/test-results`,
+          playwrightConfig: `apps/${projectName}/playwright.config.ts`,
+          baseUrl: 'https://example.com',
+          debug: true,
+        },
+        configurations: {
+          production: {
+            baseUrl: 'https://example.com',
+          },
+        },
+      });
+
+      expect(project.targets['show-report']).toEqual({
+        executor: '@nxkit/playwright:show-report',
+        options: {
+          reportPath: `dist/apps/${projectName}/playwright-report`,
+        },
+      });
+
+      expect(project.targets.lint).toEqual({
+        executor: '@nrwl/linter:eslint',
+        outputs: ['{options.outputFile}'],
+        options: {
+          lintFilePatterns: [`apps/${projectName}/**/*.{js,ts}`],
+        },
+      });
+    });
+
+    it('should generate project files', async () => {
+      const projectName = options.name + '-e2e';
+      await projectGenerator(appTree, options);
+
+      expect(
+        appTree.exists(`apps/${projectName}/playwright.config.ts`)
+      ).toBeTruthy();
+      expect(
+        appTree.exists(`apps/${projectName}/src/e2e/app.spec.ts`)
+      ).toBeTruthy();
+      expect(
+        appTree.exists(
+          `apps/${projectName}/src/tests-examples/demo-todo-app.spec.ts`
+        )
+      ).toBeTruthy();
+    });
   });
 
+  describe('nested', () => {});
+
   it('should create e2e project for a frontend app', async () => {
-    const frontendProject = 'test-app';
-    const project = frontendProject + '-e2e';
-    addProjectConfiguration(appTree, frontendProject, {
+    const frontendProjectName = 'test-app';
+    const projectName = frontendProjectName + '-e2e';
+    addProjectConfiguration(appTree, frontendProjectName, {
       root: 'my-app',
       targets: {
         serve: {
@@ -40,10 +109,12 @@ describe('project generator', () => {
 
     await projectGenerator(appTree, {
       ...options,
-      name: project,
-      frontendProject,
+      name: projectName,
+      frontendProject: frontendProjectName,
     });
-    const config = readProjectConfiguration(appTree, project);
-    expect(config).toBeDefined();
+
+    const project = readProjectConfiguration(appTree, projectName);
+    expect(project).toBeDefined();
+    expect(project.implicitDependencies).toEqual([frontendProjectName]);
   });
 });
