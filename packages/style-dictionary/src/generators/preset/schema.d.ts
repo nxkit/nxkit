bf332dc6d1ce9f72495041562c54e691b53cca7b
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2023-02-06 02:12:55 -0500

    fix(style-dictionary): add preset generator linting (#51)

--- packages/style-dictionary/src/generators/preset/schema.d.ts
+++ packages/style-dictionary/src/generators/preset/schema.d.ts
@@ -1,6 +1,8 @@
+import { Linter } from '@nrwl/linter';
 import { Preset } from '../library/schema';
 
 export interface PresetGeneratorSchema {
   name: string;
   preset: Preset;
+  linter?: Linter;
 }
