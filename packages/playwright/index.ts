1b676322b6655205a2f9b54d75b4a1bfd66ce246
Dmitriy Stepanenko <33101123+dmitry-stepanenko@users.noreply.github.com>
2023-02-11 18:27:50 +0200

    fix(core): move index.ts files to the root of the packages (#59)

---
+++ packages/playwright/index.ts
@@ -0,0 +1,15 @@
+// Test Executor
+export { testExecutor } from './src/executors/test/executor';
+export { TestExecutorSchema } from './src/executors/test/schema';
+
+// Show Report Executor
+export { showReportExecutor } from './src/executors/show-report/executor';
+export { ShowReportExecutorSchema } from './src/executors/show-report/schema';
+
+// Init Generator
+export { initGenerator } from './src/generators/init/generator';
+export { InitGeneratorSchema } from './src/generators/init/schema';
+
+// Project Generator
+export { projectGenerator } from './src/generators/project/generator';
+export { ProjectGeneratorSchema } from './src/generators/project/schema';
