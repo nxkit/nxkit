f1bd21f8ce99d89a31ab0c2c61bd9eb31e153d16
Jose Gregorio Rodriguez Chacon <jgrodriguez@addi.com>
2023-04-24 14:21:24 -0300

    cleanup(style-dictionary): tests name changes

--- e2e/style-dictionary-e2e/tests/style-dictionary.spec.ts
+++ e2e/style-dictionary-e2e/tests/style-dictionary.spec.ts
@@ -62,7 +62,7 @@ describe('style-dictionary e2e', () => {
   );
 
   it(
-    'it should create multi config project',
+    'should create multi config project',
     async () => {
       const project = uniq('style-dictionary');
       await runNxCommandAsync(
