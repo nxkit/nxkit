9265fe1328763186c937a1b8cbe7e1eff2decb31
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2024-01-30 16:42:51 +0100

    Fix 'no properties' error on Windows
    By changing the `resolve` function to the POSIX variant, the created path works with the glob function in style-dictionary that uses `{ posix: true }`.
    
    Ref https://github.com/amzn/style-dictionary/blob/17f4cb2f30bd002dfd55d6ef8c5bee4138de8d64/lib/utils/combineJSON.js#L49
    
    Fixes https://github.com/nxkit/nxkit/issues/97

--- packages/style-dictionary/src/executors/build/lib/normalize-config.ts
+++ packages/style-dictionary/src/executors/build/lib/normalize-config.ts
@@ -1,5 +1,5 @@
 import { ExecutorContext, joinPathFragments } from '@nx/devkit';
-import { resolve } from 'path';
+import { posix } from 'path';
 import type { Config, Platform } from 'style-dictionary';
 import { NormalizedBuildExecutorSchema } from '../schema';
 
@@ -11,8 +11,8 @@ export function normalizeStyleDictionaryConfig(
   const { root: projectRoot } = context.workspace.projects[context.projectName];
   const normalized: Config = {
     ...config,
-    source: config.source.map((src) => resolve(projectRoot, src)),
-    include: config?.include?.map((include) => resolve(projectRoot, include)),
+    source: config.source.map((src) => posix.resolve(projectRoot, src)),
+    include: config?.include?.map((include) => posix.resolve(projectRoot, include)),
     platforms: Object.entries(config.platforms).reduce(
       (accum: { [key: string]: Platform }, [name, platform]) => ({
         ...accum,
